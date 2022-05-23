fdac77906b1db1c03911b3c6c511a447d0cbe570
ilyasharipov <ilyasharipov>
2022-05-23 10:19:59 +0700

    a few changes

--- src/application.js
+++ src/application.js
@@ -1,43 +1,51 @@
-import onChange from 'on-change';
-import render from './view.js';
-import validate from './validation.js';
-import isEmpty from 'lodash/isEmpty.js'
+import onChange from "on-change";
+import render from "./view.js";
+import validate from "./validation.js";
+import isEmpty from "lodash/isEmpty.js";
 
 export default () => {
-    const elements = {
-        form: document.querySelector('.rss-form'),
-        fields: {
-            url: document.querySelector('#url-input'),
+  const elements = {
+    form: document.querySelector(".rss-form"),
+    fields: {
+      url: document.querySelector("#url-input"),
+    },
+  };
+
+  const state = onChange(
+    {
+      form: {
+        valid: true,
+        processState: "filling",
+        processError: null,
+        urls: [],
+      },
+    },
+    render(elements)
+  );
+
+  elements.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const urlInputValue = formData.get("url");
+
+    // state.form.fields.url = urlInputValue;
+
+    validate(state.form.fields)
+      .then((errors) => {
+        state.form.errors = errors;
+        state.form.valid = isEmpty(errors);
+      })
+      .then(() => {
+        if (!state.form.valid) {
+          console.log("not true");
+          return;
         }
-    };
 
-    const state = onChange({
-        form: {
-            valid: true,
-            processState: 'filling',
-            processError: null,
-            fields: {
-                url: '',
-            },
-        }
-    }, render(elements));
+        state.form.urls.push(urlInputValue);
+        state.form.processState = "sending";
+        state.form.processError = null;
 
-    elements.form.addEventListener('submit', (e) =>  {
-        e.preventDefault();
-
-        const formData = new FormData(e.target);
-        const urlInputValue = formData.get('url');
-
-        state.form.fields.url = urlInputValue;
-
-        validate(state.form.fields).then((errors) => {
-            console.log(errors);
-            state.form.errors = errors;
-            state.form.processState = 'sending';
-            state.form.processError = null;
-        });
-        
-        
         // try {
         //     const data = {
         //         name: state.form.fields.name,
@@ -52,5 +60,6 @@ export default () => {
         //     state.form.processError = errorMessages.network.error;
         //     throw err;
         // }
-    });
+      });
+  });
 };
--- src/view.js
+++ src/view.js
@@ -1,85 +1,92 @@
-import has from 'lodash/has.js';
+import has from "lodash/has.js";
 
 const handleProcessState = (elements, processState) => {
-    switch (processState) {
-        case 'sent':
-            // elements.container.innerHTML = 'User Created!';
-            break;
+  switch (processState) {
+    case "sent":
+      console.log("sent");
+      // elements.container.innerHTML = 'User Created!';
+      break;
 
-        case 'error':
-            // elements.submitButton.disabled = false;
-            break;
+    case "error":
+      console.log("err");
+      // elements.submitButton.disabled = false;
+      break;
 
-        case 'sending':
-            // elements.submitButton.disabled = true;
-            break;
+    case "sending":
+      console.log("sending");
+      // elements.submitButton.disabled = true;
+      break;
 
-        case 'filling':
-            // elements.submitButton.disabled = false;
-            break;
+    case "filling":
+      console.log("fill!");
+      // elements.submitButton.disabled = false;
+      break;
 
-        default:
-            // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
-            throw new Error(`Unknown process state: ${processState}`);
-    }
+    default:
+      // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
+      throw new Error(`Unknown process state: ${processState}`);
+  }
 };
 
 const handleProcessError = () => {
-    // вывести сообщение о сетевой ошибке
+  // вывести сообщение о сетевой ошибке
 };
 
 const renderErrors = (elements, errors, prevErrors) => {
-    const formBlockElement = elements.form.closest('div');
-    Object.entries(elements.fields).forEach(([fieldName, fieldElement]) => {
-        const error = errors[fieldName];
+  const formBlockElement = elements.form.closest("div");
+  Object.entries(elements.fields).forEach(([fieldName, fieldElement]) => {
+    const error = errors[fieldName];
 
-        const fieldHadError = has(prevErrors, fieldName);
-        const fieldHasError = has(errors, fieldName);
-        if (!fieldHadError && !fieldHasError) {
-            return;
-        }
+    const fieldHadError = has(prevErrors, fieldName);
+    const fieldHasError = has(errors, fieldName);
+    if (!fieldHadError && !fieldHasError) {
+      return;
+    }
 
-        if (fieldHadError && !fieldHasError) {
-            fieldElement.classList.remove('is-invalid');
-            formBlockElement.lastElementChild.remove();
-            return;
-        }
+    if (fieldHadError && !fieldHasError) {
+      fieldElement.classList.remove("is-invalid");
+      formBlockElement.lastElementChild.remove();
+      return;
+    }
 
-        if (fieldHadError && fieldHasError) {
-            const feedbackElement = formBlockElement.lastElementChild;
-            feedbackElement.textContent = error.message;
-            return;
-        }
+    if (fieldHadError && fieldHasError) {
+      const feedbackElement = formBlockElement.lastElementChild;
+      feedbackElement.textContent = error.message;
+      return;
+    }
 
-        fieldElement.classList.add('is-invalid');
-        const feedbackElement = document.createElement('div');
-        feedbackElement.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-danger');
-        feedbackElement.textContent = error.message;
-        
-        // fieldElement.after(feedbackElement);
-        formBlockElement.append(feedbackElement);
-    });
+    fieldElement.classList.add("is-invalid");
+    const feedbackElement = document.createElement("div");
+    feedbackElement.classList.add(
+      "feedback",
+      "m-0",
+      "position-absolute",
+      "small",
+      "text-danger"
+    );
+    feedbackElement.textContent = error.message;
+
+    // fieldElement.after(feedbackElement);
+    formBlockElement.append(feedbackElement);
+  });
 };
 
 export default (elements) => (path, value, prevValue) => {
-    switch (path) {
-        case 'form.processState':
-            console.log('process state DA!')
-            handleProcessState(elements, value);
-            break;
+  switch (path) {
+    case "form.processState":
+      handleProcessState(elements, value);
+      break;
 
-        case 'form.processError':
-            console.log('process eror DA!')
-            handleProcessError();
-            break;
+    case "form.processError":
+      handleProcessError();
+      break;
 
-        case 'form.errors':
-            console.log('EROR DA?')
-            console.log(prevValue)
-            renderErrors(elements, value, prevValue);
-            break;
+    case "form.errors":
+      console.log(prevValue);
+      renderErrors(elements, value, prevValue);
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 };
