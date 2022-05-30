d326b78fb9a79e3d64d6f2c8fab0552b9e20a640
Ilya Sharipov <ilyas@evaxsoft.com>
2022-05-30 12:31:28 +0700

    change logic

--- src/application.js
+++ src/application.js
@@ -7,74 +7,76 @@ import resources from "./locales/index.js";
 
 const languages = ["en", "ru"];
 
-export default () => {
-  const i18nInstance = i18n.createInstance();
-
-  i18nInstance.init({
-    lng: "en",
-    debug: false,
-    resources,
-  });
-
-  // console.log(i18nInstance.t("languages.ru"));
-  const elements = {
-    form: document.querySelector(".rss-form"),
-    fields: {
-      url: document.querySelector("#url-input"),
-    },
-  };
-
-  const state = onChange(
-    {
-      form: {
-        valid: true,
+export default (initialState) => {
+    // console.log(i18nInstance.t("languages.ru"));
+    const elements = {
+        form: document.querySelector(".rss-form"),
         fields: {
-          url: "",
+            url: document.querySelector("#url-input"),
         },
-        processState: "filling",
-        processError: null,
-        urls: [],
-      },
-    },
-    render(elements, i18nInstance)
-  );
+    };
 
-  elements.form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    const defaultLanguage = 'ru';
+    const i18nInstance = i18n.createInstance();
 
-    const formData = new FormData(e.target);
-    const urlInputValue = formData.get("url");
-    console.log(urlInputValue);
-    state.form.fields.url = urlInputValue;
-    validate(state.form)
-      .then((errors) => {
-        state.form.errors = errors;
-        state.form.valid = isEmpty(errors);
-      })
-      .then(() => {
-        if (!state.form.valid) {
-          console.log("not true");
-          return;
-        }
+    const state = {
+        lng: defaultLanguage,
+        form: {
+            valid: true,
+            fields: {
+                url: "",
+            },
+            processState: "filling",
+            processError: null,
+            urls: [],
+        },
+        ...initialState
+    };
+    
+    i18nInstance.init({
+        lng: state.lng,
+        debug: false,
+        resources,
+    });
 
-        state.form.urls.push(urlInputValue);
-        state.form.processState = "sending";
-        state.form.processError = null;
+    onChange(state, render(elements, i18nInstance));
+    
+    elements.form.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-        // try {
-        //     const data = {
-        //         name: state.form.fields.name,
-        //         email: state.form.fields.email,
-        //         password: state.form.fields.password,
-        //     };
-        //     await axios.post(routes.usersPath(), data);
-        //     state.form.processState = 'sent';
-        // } catch (err) {
-        //     // в реальных приложениях необходимо помнить об обработке ошибок сети
-        //     state.form.processState = 'error';
-        //     state.form.processError = errorMessages.network.error;
-        //     throw err;
-        // }
-      });
-  });
+        const formData = new FormData(e.target);
+        const urlInputValue = formData.get("url");
+        console.log(urlInputValue);
+        state.form.fields.url = urlInputValue;
+        validate(state.form)
+            .then((errors) => {
+                state.form.errors = errors;
+                state.form.valid = isEmpty(errors);
+            })
+            .then(() => {
+                if (!state.form.valid) {
+                    console.log("not true");
+                    return;
+                }
+
+                state.form.urls.push(urlInputValue);
+                state.form.processState = "sending";
+                state.form.processError = null;
+
+                // try {
+                //     const data = {
+                //         name: state.form.fields.name,
+                //         email: state.form.fields.email,
+                //         password: state.form.fields.password,
+                //     };
+                //     await axios.post(routes.usersPath(), data);
+                //     state.form.processState = 'sent';
+                // } catch (err) {
+                //     // в реальных приложениях необходимо помнить об обработке ошибок сети
+                //     state.form.processState = 'error';
+                //     state.form.processError = errorMessages.network.error;
+                //     throw err;
+                // }
+            });
+    });
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import  app from './application';
 
-app();
+const state = {
+    lng: 'ru',
+};
+
+
+app(state);
